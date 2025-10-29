0d3873388d4f898ec7b36cb5af3b0a7cfbae4168
avezedroos <154615446+avezedroos@users.noreply.github.com>
2025-10-29 12:29:26 +0530

    updated Api
    here i update the Api endpoint

--- my-chat-app/src/services/api.js
+++ my-chat-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://chatting-app-backend-ofme.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
